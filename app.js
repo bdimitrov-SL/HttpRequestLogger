8ad14f7dc00f86905306ef7ac6236d67b5a6011f
Bogdan Dimitrov <BDimitrov@scottlogic.com>
2015-11-26 10:27:14 +0000

    Changed port again....

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ var server = http.createServer(function (request, response) {
 		winston.info(request);
 		response.end();
 	});
-}).listen(55555);
+}).listen(process.env.PORT || 55555);
 
 console.log('Http Request Logger is running.');
